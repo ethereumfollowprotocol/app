49137e0c3847bf877accad9544f5fa129e4a206c
Omar <github@omaraziz.dev>
2023-09-16 22:28:48 -0700

    enable build in ci

--- apps/web/src/app/[locale]/client.tsx
+++ apps/web/src/app/[locale]/client.tsx
@@ -5,8 +5,8 @@ import type { ReactNode } from 'react'
 import { Theme } from '@radix-ui/themes'
 import { wagmiConfig } from '#/lib/wallet'
 import { ConnectKitProvider } from 'connectkit'
-import { Header } from '#/components/header'
-import { Footer } from '#/components/footer'
+import { Header } from '#/components/header.tsx'
+import { Footer } from '#/components/footer.tsx'
 import { I18nProviderClient } from '#/locales/client'
 
 export default function Layout({ children }: { children: ReactNode }) {
