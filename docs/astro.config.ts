6d2bb6a065042a0d80720eccb3ae1a41fa0fdf47
Omar <github@omaraziz.dev>
2023-09-21 20:25:38 -0700

    docs: font

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -1,16 +1,17 @@
-import starlight from '@astrojs/starlight'
 import tailwind from '@astrojs/tailwind'
-import vercelAdapter from '@astrojs/vercel/serverless'
+import starlight from '@astrojs/starlight'
 import expressiveCode from 'astro-expressive-code'
+import vercelAdapter from '@astrojs/vercel/serverless'
 import { defineConfig, passthroughImageService } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://docs.ethfollow.xyz',
-  output: 'hybrid',
-  adapter: vercelAdapter({
-    webAnalytics: { enabled: true },
-  }),
+  /**
+   * For IPFS (fleek.xyz), `output` should be `static`
+   */
+  output: 'server',
+  adapter: vercelAdapter({ webAnalytics: { enabled: true } }),
   integrations: [
     expressiveCode({
       theme: 'dracula-soft',
@@ -45,6 +46,8 @@ export default defineConfig({
         '@fontsource/inter/700.css',
         '@fontsource/ibm-plex-serif/400.css',
         '@fontsource/ibm-plex-serif/600.css',
+        '@fontsource/ibm-plex-mono/400.css',
+        '@fontsource/ibm-plex-mono/600.css',
       ],
     }),
   ],
