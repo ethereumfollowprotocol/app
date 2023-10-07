95c75927ec26dc6d01b4c9a5ae2266be7fd6fac3
Omar <github@omaraziz.dev>
2023-10-06 18:11:19 -0700

    sst open-next

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-# Find all node_modules folders and remove them recursively
 echo "Removing all node_modules folders..."
 find . -name 'node_modules' -exec rm -rf {} \;
 
-# Find all dist folders and remove them recursively
 echo "Removing all dist folders..."
 find . -name 'dist' -exec rm -rf {} \;
 
@@ -16,7 +14,5 @@ find . -name '.turbo' -exec rm -rf {} \;
 
 echo "Removing all .next files..."
 find . -name '.next' -exec rm -rf {} \;
-
-rm -rf pnpm-out out
 
 echo "Done."
---
+++ sst.config.ts
@@ -0,0 +1,15 @@
+import type { SSTConfig } from 'sst'
+import { NextjsSite } from 'sst/constructs'
+
+export default {
+  config: _input => ({ name: 'efp', region: 'us-east-1' }),
+  stacks: app => {
+    app.stack(function Site({ stack }) {
+      const site = new NextjsSite(stack, 'site')
+
+      stack.addOutputs({
+        SiteUrl: site.url,
+      })
+    })
+  },
+} satisfies SSTConfig
