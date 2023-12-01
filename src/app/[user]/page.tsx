ce6721f8fa1dfdc95eb8b537309e41a4644786ac
Omar <github@omaraziz.dev>
2023-12-01 13:35:27 -0800

    use next/image

--- src/app/[user]/page.tsx
+++ src/app/[user]/page.tsx
@@ -1,17 +1,16 @@
-import { Avatar } from '@radix-ui/themes'
+import Image from 'next/image'
 import { getEnsProfile } from 'src/app/actions.ts'
 
 export default async function Page({ params }: { params: { user: string } }) {
   const profile = await getEnsProfile(params.user)
   return (
     <main className='font-sans mx-auto flex h-full min-h-full w-full flex-col items-center overflow-scroll mb-12 px-4 pt-6 text-center'>
-      <Avatar
+      <Image
         src={profile['avatar']}
-        fallback=''
         alt='env ave'
         width={200}
         height={200}
-        radius='full'
+        className='rounded-xl'
       />
       <section className='max-w-xl'>
         <pre className='text-left text-black text-clip overflow-clip'>
