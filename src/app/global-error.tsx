fae674105be0e064c1ce99011509c1834a502003
Omar <github@omaraziz.dev>
2023-12-19 01:10:59 -0800

    feat: sentry global error

---
+++ src/app/global-error.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import * as React from 'react'
+import * as Sentry from '@sentry/nextjs'
+import { default as NextError } from 'next/error'
+
+export default function GlobalError({ error }: { error: NextError }) {
+  React.useEffect(() => {
+    Sentry.captureException(error)
+  }, [error])
+
+  return (
+    <html lang='en'>
+      <body>
+        {/* This is the default Next.js error component but it doesn't allow omitting the statusCode property yet. */}
+        <NextError statusCode={undefined as any} />
+      </body>
+    </html>
+  )
+}
