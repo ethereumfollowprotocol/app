f9d03bcfd96faf97cb97e58e34680385ab01bfb6
Omar <github@omaraziz.dev>
2023-12-20 19:27:44 -0800

    server action teams page

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,41 +1,20 @@
 import * as React from 'react'
-import { DotsHorizontalIcon } from '@radix-ui/react-icons'
+import { getEnsProfile } from '#app/actions.ts'
+import { teamAddresses } from 'src/lib/constants'
 import { FollowButton } from 'src/components/follow-button.tsx'
-import { Avatar, Badge, Box, Flex, IconButton, Text } from '@radix-ui/themes'
-
-const efpTeam = [
-  {
-    ens: 'brantly.eth',
-    address: '0x983110309620D911731Ac0932219af06091b6744',
-    x: 'https://x.com/BrantlyMillegan',
-    github: 'https://github.com/BrantlyMillegan',
-    efp: ''
-  },
-  {
-    ens: 'cory.eth',
-    address: '0xBdB41BfF7E828E2DC2d15EB67257455db818F1DC',
-    x: 'https://x.com/cory_eth',
-    github: 'https://github.com/stoooops',
-    efp: ''
-  },
-  {
-    ens: 'esm.eth',
-    address: '0xf4212614C7Fe0B3feef75057E88b2E77a7E23e83',
-    x: 'https://x.com/awkroot',
-    github: 'https://github.com/o-az',
-    efp: ''
-  }
-] satisfies Array<TeamMember>
+import { Avatar, Badge, Box, Flex, Text } from '@radix-ui/themes'
 
 interface TeamMember {
   ens: string
   address: string
   x: string
   github: string
-  efp: string
+  avatar: string
 }
 
 export default async function TeamPage() {
+  const efpTeam = await Promise.all(teamAddresses.map(getEnsProfile))
+
   return (
     <main className='mx-auto flex h-full min-h-full w-full flex-col items-center overflow-scroll mb-12 px-4 pt-6 text-center'>
       <Text size='7' className='font-bold' mb='5'>
@@ -45,9 +24,15 @@ export default async function TeamPage() {
         mx='auto'
         className='flex-col lg:flex-row lg:gap-y-0 gap-y-6 space-x-0 md:space-x-12 align-middle justify-center items-center'
       >
-        {efpTeam.map(({ ens, address, efp, x, github }) => (
-          <div key={ens}>
-            <TeamCard ens={ens} address={address} github={github} x={x} efp={efp} />
+        {efpTeam.map(({ name, address, records, avatar }) => (
+          <div key={address}>
+            <TeamCard
+              ens={name}
+              address={address}
+              avatar={avatar}
+              x={records['com.twitter'] ?? ''}
+              github={records['com.github'] ?? ''}
+            />
           </div>
         ))}
       </Flex>
@@ -56,19 +41,14 @@ export default async function TeamPage() {
 }
 
 function TeamCard(props: TeamMember) {
-  const { ens, address, efp, x, github } = props
+  const { ens, address, avatar, x, github } = props
   return (
     <Flex direction='column'>
       <Flex mx='auto' className='bg-white/70 border-0 max-w-80 w-[295px] min-w-60 rounded-xl p-3'>
         <Flex direction='column' align='start' width='100%' height='100%' justify='center'>
           <Badge>#132</Badge>
           <Flex direction='column' justify='center' align='center' mx='auto' mt='3'>
-            <Avatar
-              src={`https://metadata.ens.domains/mainnet/avatar/${ens}`}
-              radius='full'
-              size='7'
-              fallback=''
-            />
+            <Avatar src={avatar} radius='full' size='7' fallback='' />
             <Text size='5' className='font-bold' my='2'>
               {ens}
             </Text>
--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -7,6 +7,12 @@ export const APP_URL =
 
 export const ENS_SUBGRAPH = `https://api.thegraph.com/subgraphs/name/ensdomains/ens`
 
+export const teamAddresses = [
+  '0x983110309620D911731Ac0932219af06091b6744',
+  '0xBdB41BfF7E828E2DC2d15EB67257455db818F1DC',
+  '0xf4212614C7Fe0B3feef75057E88b2E77a7E23e83'
+]
+
 export const SECOND = 1_000
 export const MINUTE = 60 * SECOND
 export const HOUR = 60 * MINUTE
