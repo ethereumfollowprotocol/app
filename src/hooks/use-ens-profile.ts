67e14fbece67922fa813918ffa333aa9d6197501
omar <omaraziz.dev@proton.me>
2024-01-17 20:17:39 -0800

    wip: cool card

--- src/hooks/use-ens-profile.ts
+++ src/hooks/use-ens-profile.ts
@@ -1,12 +1,13 @@
-import { isAddress } from 'viem'
+
 import { getEnsProfile } from '#/app/actions.ts'
 import { useQuery } from '@tanstack/react-query'
+import { checkAddressOrEnsValid } from '#/lib/utilities.ts'
 
 export function useEnsProfile(addressOrName?: string) {
   return useQuery({
     queryKey: ['ens-profile', addressOrName],
     queryFn: async () => getEnsProfile(`${addressOrName}`),
-    enabled: !!addressOrName && isAddress(addressOrName),
+    enabled: !!addressOrName && checkAddressOrEnsValid(addressOrName),
     refetchOnWindowFocus: false
   })
 }
