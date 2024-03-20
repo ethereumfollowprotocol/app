83ec27fe9724740fab1f12c269ffd60124bcad3a
marcomariscal <marco.a.mariscal@gmail.com>
2024-03-20 10:38:29 -0700

    fix: use followers that aren't being followed as recommendations for now

--- src/app/editor/recommendations.tsx
+++ src/app/editor/recommendations.tsx
@@ -1,54 +1,27 @@
-import Link from 'next/link'
-import { Flex, Heading, Text } from '@radix-ui/themes'
-import { Avatar } from '#/components/avatar'
-import { FollowButton } from '#/components/follow-button'
-
-// Mock data or prop structure for recommendation items
-interface Recommendation {
-  id: string
-  address: `0x${string}`
-  name: string
-  avatarUrl?: string // Optional avatar URL
-}
-
-// Example recommendations data or fetched from an API
-const recommendations: Recommendation[] = [
-  { id: '1', name: 'Jane Doe', avatarUrl: '/path/to/avatar1.jpg', address: '0x1234567890' },
-  { id: '2', name: 'John Smith', avatarUrl: '/path/to/avatar2.jpg', address: '0x1234567891' }
-]
+import { FollowList } from '#/components/follow-list'
+import { useConnectedProfile } from '#/api/actions'
+import { useMemo } from 'react'
 
 interface RecommendationsProps {
   header: string
 }
 
 export function Recommendations({ header }: RecommendationsProps) {
-  return (
-    <Flex gap='4' direction='column' className='w-full'>
-      <Heading size='5' as='h2' className='text-left font-bold my-2'>
-        {header}
-      </Heading>
-      <Flex gap='4' direction='column'>
-        {recommendations.map(recommendation => (
-          <RecommendationItem key={recommendation.id} recommendation={recommendation} />
-        ))}
-      </Flex>
-    </Flex>
-  )
-}
+  const { profile } = useConnectedProfile()
 
-function RecommendationItem({ recommendation }: { recommendation: Recommendation }) {
-  const { name, avatarUrl, address } = recommendation
-  return (
-    <Flex className='items-center justify-between'>
-      <Flex className='gap-2 items-center'>
-        <Avatar name={name} avatarUrl={avatarUrl} />
-        <Link href={`/${name}`} passHref={true}>
-          <Text as='p' className='font-bold xl:text-lg lg:text-md text-sm hover:text-pink-400'>
-            {name}
-          </Text>
-        </Link>
-      </Flex>
-      <FollowButton address={address} />
-    </Flex>
-  )
+  // TODO get better recommendations
+  // Currently using the followers that aren't being followed by the connected profile
+  const followerAddresses = profile?.followerAddresses || []
+  const followingAddresses = profile?.followingAddresses || []
+
+  // Filter out followers that are already being followed
+  const profilesToRecommend = useMemo(() => {
+    return followerAddresses
+      .filter(follower => !followingAddresses.includes(follower))
+      .map(address => ({ address, tags: [] }))
+  }, [followerAddresses, followingAddresses])
+
+  if (!profilesToRecommend) return null
+
+  return <FollowList profiles={profilesToRecommend} showTags={false} showFollowsYouBadges={true} />
 }
