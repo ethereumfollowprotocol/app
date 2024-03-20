dffdfac6ba76c0c5404cccb11992383fe2577285
marcomariscal <marco.a.mariscal@gmail.com>
2024-03-20 10:16:11 -0700

    feat: confirm changes button

--- src/app/editor/unconfirmed-changes.tsx
+++ src/app/editor/unconfirmed-changes.tsx
@@ -1,14 +1,13 @@
 import { useConnectedProfile } from '#/api/actions'
-import { fetchUserProfile } from '#/api/requests'
 import { FollowList, type FollowListProfile } from '#/components/follow-list'
 import { useCart } from '#/contexts/cart-context'
 import { hexlify } from '#/lib/utilities'
+import { Box, Button, Text } from '@radix-ui/themes'
 import { useMemo } from 'react'
-import { getAddress } from 'viem'
 
 export function UnconfirmedChanges() {
   const { profile: connectedProfile } = useConnectedProfile()
-  const { cartItems } = useCart()
+  const { cartItems, totalCartItems } = useCart()
 
   // map the cart items to the FollowListProfile type
   // use the op add tag, remove tag, add list, remove list to determine the operation
@@ -21,14 +20,45 @@ export function UnconfirmedChanges() {
     [cartItems]
   )
 
+  const handleConfirm = () => {
+    console.log('Confirming changes')
+  }
+
   if (!connectedProfile) return null
   if (!cartItems.length) return null // TODO handle no items in cart
 
   return (
-    <FollowList
-      profiles={unconfirmedProfiles}
-      listClassName='gap-2 p-4 rounded-xl bg-white/50'
-      listItemClassName='rounded-xl p-2'
-    />
+    <>
+      <FollowList
+        profiles={unconfirmedProfiles}
+        listClassName='gap-2 p-4 rounded-xl bg-white/50'
+        listItemClassName='rounded-xl p-2'
+      />
+      <Box className='mr-8 flex justify-end'>
+        <Box className='flex gap-6 p-6 bg-white rounded-xl'>
+          <Box className='flex gap-2 items-center'>
+            <Text size='8' weight='bold'>
+              {totalCartItems}
+            </Text>
+            <Box className='flex flex-col text-right'>
+              <Text size='1' weight='bold'>
+                Unconfirmed
+              </Text>
+              <Text size='1' weight='bold'>
+                Changes
+              </Text>
+            </Box>
+          </Box>
+          <Button
+            className='bg-gradient-to-b from-kournikova-300 to-salmon-400 text-black'
+            radius='full'
+            onClick={handleConfirm}
+            size='3'
+          >
+            Confirm
+          </Button>
+        </Box>
+      </Box>
+    </>
   )
 }
