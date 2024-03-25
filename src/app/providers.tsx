156df5c1d4f4cfb9b2a62f79914a55d9324e7806
marcomariscal <marco.a.mariscal@gmail.com>
2024-03-25 09:57:21 -0700

    fix: remove unused

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,7 +2,6 @@
 
 import { Header } from '#/components/header.tsx'
 import { CartProvider } from '#/contexts/cart-context'
-import { SelectedProfileProvider } from '#/contexts/selected-profile-context'
 import { DAY, MINUTE } from '#/lib/constants'
 import { wagmiConfig } from '#/lib/wagmi'
 import { Theme } from '@radix-ui/themes'
@@ -40,16 +39,14 @@ export function Providers({ children, initialState }: Props) {
         <WagmiProvider config={wagmiConfig} reconnectOnMount={true} initialState={initialState}>
           {/* <PersistQueryClientProvider client={queryClient} persistOptions={{ persister }}> */}
           <RainbowKitProvider coolMode={true} initialChain={1}>
-            <SelectedProfileProvider>
-              <CartProvider>
-                <Theme scaling='100%' appearance='inherit' accentColor='gray'>
-                  <React.Suspense>
-                    <Header />
-                  </React.Suspense>
-                  {children}
-                </Theme>
-              </CartProvider>
-            </SelectedProfileProvider>
+            <CartProvider>
+              <Theme scaling='100%' appearance='inherit' accentColor='gray'>
+                <React.Suspense>
+                  <Header />
+                </React.Suspense>
+                {children}
+              </Theme>
+            </CartProvider>
           </RainbowKitProvider>
           {/* </PersistQueryClientProvider> */}
         </WagmiProvider>
