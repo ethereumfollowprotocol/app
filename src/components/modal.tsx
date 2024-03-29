c2a7e76b4be78a4529fccce59daa63075cf2cfc9
marcomariscal <marco.a.mariscal@gmail.com>
2024-03-29 14:23:46 -0700

    fix: no nested button

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,5 +1,5 @@
 import { Cross1Icon } from '@radix-ui/react-icons'
-import { IconButton } from '@radix-ui/themes'
+import { Box } from '@radix-ui/themes'
 import { Dialog } from '@radix-ui/themes'
 import { DialogTrigger } from '@radix-ui/react-dialog'
 import type { ReactNode } from 'react'
@@ -15,9 +15,9 @@ export function Modal({ children, triggerButton }: ModalProps) {
       <DialogTrigger>{triggerButton}</DialogTrigger>
       <Dialog.Content className='inset-0 m-auto flex flex-col items-center rounded-xl p-10 bg-white shadow-lg relative overflow-visible w-[522px] h-[724px]'>
         <Dialog.Close>
-          <IconButton className='absolute top-[-12px] right-[-12px] text-gray-500 hover:text-gray-700 rounded-full bg-white p-1 shadow-lg'>
-            <Cross1Icon />
-          </IconButton>
+          <Box className='absolute top-[-12px] right-[-12px] text-gray-500 hover:text-gray-700 rounded-full bg-white p-2 shadow-lg hover:cursor-pointer'>
+            <Cross1Icon className='h-5 w-5' />
+          </Box>
         </Dialog.Close>
         {children}
       </Dialog.Content>
