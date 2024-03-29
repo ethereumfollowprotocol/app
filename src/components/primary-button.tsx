0698d6637acf4d56d7a53b9ca29e5aefca885266
marcomariscal <marco.a.mariscal@gmail.com>
2024-03-29 14:23:29 -0700

    feat: use additional props

--- src/components/primary-button.tsx
+++ src/components/primary-button.tsx
@@ -1,19 +1,24 @@
 import { Button } from '@radix-ui/themes'
 import clsx from 'clsx'
+import type { ComponentPropsWithoutRef } from 'react'
 
-interface PrimaryButtonProps {
+interface PrimaryButtonProps extends ComponentPropsWithoutRef<typeof Button> {
   className?: string
   label: string
-  onClick?: () => void
 }
 
-export function PrimaryButton({ className, label, onClick }: PrimaryButtonProps) {
+export function PrimaryButton({ className, label, onClick, ...props }: PrimaryButtonProps) {
   return (
     <Button
       onClick={onClick}
-      className={clsx('bg-gradient-to-b from-kournikova-300 to-salmon-400 text-black', className)}
+      className={clsx(
+        'bg-gradient-to-b from-kournikova-300 to-salmon-400 text-black',
+        props.disabled && 'cursor-not-allowed',
+        className
+      )}
       radius='full'
       size='3'
+      {...props}
     >
       {label}
     </Button>
