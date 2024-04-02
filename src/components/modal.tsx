e6a274152bb5103f0c4692dd4450a4a6e7b41a37
marcomariscal <marco.a.mariscal@gmail.com>
2024-04-02 09:48:54 -0700

    fix: forward ref

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -7,12 +7,13 @@ import type { ReactNode } from 'react'
 interface ModalProps {
   children: ReactNode
   triggerButton: ReactNode
+  triggerButtonClassName?: string
 }
 
 export function Modal({ children, triggerButton }: ModalProps) {
   return (
     <Dialog.Root>
-      <DialogTrigger>{triggerButton}</DialogTrigger>
+      <DialogTrigger asChild={true}>{triggerButton}</DialogTrigger>
       <Dialog.Content className='inset-0 m-auto flex flex-col items-center rounded-xl p-10 bg-white shadow-lg relative overflow-visible w-[522px] h-[724px]'>
         <Dialog.Close>
           <Box className='absolute top-[-12px] right-[-12px] text-gray-500 hover:text-gray-700 rounded-full bg-white p-2 shadow-lg hover:cursor-pointer'>
--- src/components/primary-button.tsx
+++ src/components/primary-button.tsx
@@ -1,26 +1,29 @@
 import { Button } from '@radix-ui/themes'
 import clsx from 'clsx'
-import type { ComponentPropsWithoutRef } from 'react'
+import { forwardRef, type ComponentPropsWithoutRef } from 'react'
 
 interface PrimaryButtonProps extends ComponentPropsWithoutRef<typeof Button> {
   className?: string
   label: string
 }
 
-export function PrimaryButton({ className, label, onClick, ...props }: PrimaryButtonProps) {
-  return (
-    <Button
-      onClick={onClick}
-      className={clsx(
-        'bg-gradient-to-b from-kournikova-300 to-salmon-400 text-black',
-        props.disabled && 'cursor-not-allowed',
-        className
-      )}
-      radius='full'
-      size='3'
-      {...props}
-    >
-      {label}
-    </Button>
-  )
-}
+export const PrimaryButton = forwardRef<HTMLButtonElement, PrimaryButtonProps>(
+  ({ className, label, onClick, ...props }, ref) => {
+    return (
+      <Button
+        ref={ref}
+        onClick={onClick}
+        className={clsx(
+          'bg-gradient-to-b from-kournikova-300 to-salmon-400 text-black',
+          props.disabled && 'cursor-not-allowed',
+          className
+        )}
+        radius='full'
+        size='3'
+        {...props}
+      >
+        {label}
+      </Button>
+    )
+  }
+)
