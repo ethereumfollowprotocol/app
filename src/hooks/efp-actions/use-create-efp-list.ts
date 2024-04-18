100151990d24e4daf8081e57843023562942d051
marcomariscal <marco.a.mariscal@gmail.com>
2024-04-18 10:25:58 -0700

    feat: comments for clarity

--- src/hooks/efp-actions/use-create-efp-list.ts
+++ src/hooks/efp-actions/use-create-efp-list.ts
@@ -1,3 +1,4 @@
+import type { ListStorageLocation } from '#/app/efp/types'
 import { generateListStorageLocationSlot } from '#/app/efp/utilities'
 import { efpListRegistryAbi } from '#/lib/abi'
 import { efpContracts } from '#/lib/constants/contracts'
@@ -5,15 +6,32 @@ import { useCallback, useMemo } from 'react'
 import { encodePacked } from 'viem'
 import { useChainId, useSimulateContract, useSwitchChain, useWriteContract } from 'wagmi'
 
+// Chain ID to use when no chain ID is provided
 const NO_CHAIN_ID = 0
 
+/**
+ * @description Create an EFP list by calling the mint function on the EFPListRegistry contract
+ * This mints an nft to the user while setting the EFP list storage location
+ * The List Storage Location value is stored in the main EFP List Registry contract and can be changed at any time by the EFP List NFT owner.
+ * @param param0
+ * @returns
+ */
 export const useCreateEFPList = ({ chainId }: { chainId: number | undefined }) => {
   const currentChainId = useChainId()
   const chainIdToUseForCreate = chainId || NO_CHAIN_ID
   const { switchChain } = useSwitchChain()
 
-  // Keep the same nonce between renders
-  const nonce = useMemo(() => generateListStorageLocationSlot(), [])
+  // EFP List Storage Location version value, which is currently always 1
+  const version: ListStorageLocation['version'] = 1
+  // EFP List Storage Location locationType value, which is currently always 1
+  const locationType: ListStorageLocation['locationType'] = 1
+  // Keep the slot the same between renders
+  const slot = useMemo(() => generateListStorageLocationSlot(), [])
+
+  const listStorageLocation = encodePacked(
+    ['uint8', 'uint8', 'uint256', 'address', 'uint'],
+    [version, locationType, BigInt(chainIdToUseForCreate), efpContracts['EFPListRecords'], slot]
+  )
 
   // Simulate the creation of an EFP list
   // If there's no chain ID, supply an invalid chain id (NO_CHAIN_ID) to force an error
@@ -26,12 +44,7 @@ export const useCreateEFPList = ({ chainId }: { chainId: number | undefined }) =
     address: efpContracts['EFPListRegistry'],
     abi: efpListRegistryAbi,
     functionName: 'mint',
-    args: [
-      encodePacked(
-        ['uint8', 'uint8', 'uint256', 'address', 'uint'],
-        [1, 1, BigInt(chainIdToUseForCreate), efpContracts['EFPListRecords'], nonce]
-      )
-    ]
+    args: [listStorageLocation]
   })
 
   const {
