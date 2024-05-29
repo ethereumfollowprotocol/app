b03d3205f7fde50a2b82565895e71dc61191ced2
encryptedDegen <encrypted@ethfollow.xyz>
2024-05-30 01:50:11 +0200

    Fix build errors

--- src/app/onboarding/page.tsx
+++
@@ -1,9 +0,0 @@
-import { CreateNewListForm } from '#/app/onboarding/new-list.tsx'
-
-export default function OnboardingPage() {
-  return (
-    <main className='mx-auto flex h-full w-full flex-col items-center text-center mt-10'>
-      <CreateNewListForm />
-    </main>
-  )
-}
