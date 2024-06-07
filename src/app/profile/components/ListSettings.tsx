95cb3543cb94cc91b552a580a0db9bfb93421987
encryptedDegen <encrypted@ethfollow.xyz>
2024-06-07 11:44:38 +0200

    Create list settings modal, fetch storage location & owner

---
+++ src/app/profile/components/ListSettings.tsx
@@ -0,0 +1,142 @@
+import type { ProfileDetailsResponse } from '#/api/requests'
+import CancelButton from '#/components/cancel-button'
+import { ChainIcon } from '#/components/chain-icon'
+import { PrimaryButton } from '#/components/primary-button'
+import type { ChainWithDetails } from '#/lib/wagmi'
+import { useTranslation } from 'react-i18next'
+import { useChains } from 'wagmi'
+import ArrowDown from 'public/assets/icons/arrow-down.svg'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import { createPublicClient, fromHex, getContract, http, type Address, type Chain } from 'viem'
+import { efpContracts } from '#/lib/constants/contracts'
+import { efpListRegistryAbi } from '#/lib/abi'
+import { optimismSepolia } from 'viem/chains'
+
+interface ListSettingsProps {
+  onClose: () => void
+  profile: ProfileDetailsResponse
+}
+
+const ListSettings: React.FC<ListSettingsProps> = ({ onClose, profile }) => {
+  const [chain, setChain] = useState<Chain | null>(null)
+  const [owner, setOwner] = useState<string>()
+  const [manager, setManager] = useState<string>()
+  const [user, setUser] = useState<string>()
+  const [changedValues, setChangedValues] = useState({
+    owner: false,
+    manager: false,
+    user: false
+  })
+  const { t } = useTranslation('profile')
+  const chains = useChains()
+
+  const listRegistryContract = getContract({
+    address: efpContracts.EFPListRegistry,
+    abi: efpListRegistryAbi,
+    client: createPublicClient({ chain: optimismSepolia, transport: http() })
+  })
+
+  const fetchListData = async () => {
+    if (!profile.primary_list) return
+
+    const listOwner = await listRegistryContract.read.ownerOf([BigInt(profile?.primary_list)])
+    const listStorageLocationChainId = fromHex(
+      `0x${(
+        await listRegistryContract.read.getListStorageLocation([BigInt(profile?.primary_list)])
+      ).slice(64, 70)}`,
+      'number'
+    )
+
+    const listStorageLocationChain = chains.find(item => item.id === listStorageLocationChainId)
+    if (listStorageLocationChain) setChain(listStorageLocationChain)
+    if (listOwner) setOwner(listOwner)
+  }
+
+  useEffect(() => {
+    fetchListData()
+  }, [profile])
+
+  return (
+    <div className='fixed z-50 top-0 flex items-center justify-center left-0 w-full h-full bg-black/50'>
+      <div className='glass-card bg-white/40 gap-8 flex flex-col rounded-xl p-10 w-[554px]'>
+        <div>
+          <h3 className='text-5xl font-semibold'>List #{profile.primary_list}</h3>
+        </div>
+        <div className='flex w-full items-center justify-between'>
+          <div>
+            <div className='text-xl text-center sm:text-2xl font-bold'>
+              {profile.stats === undefined ? '-' : profile.stats.following_count}
+            </div>
+            <div className='sm:text-lg font-bold text-gray-500'>{t('following')}</div>
+          </div>
+          <div>
+            <div className='text-xl text-center sm:text-2xl font-bold'>
+              {profile.stats === undefined ? '-' : profile.stats.followers_count}
+            </div>
+            <div className='sm:text-lg text-gray-500 font-bold'>{t('followers')}</div>
+          </div>
+          <div>
+            <div className='text-xl text-center sm:text-2xl font-bold'># -</div>
+            <div className='sm:text-lg font-bold text-gray-500'>{t('leaderboard')}</div>
+          </div>
+        </div>
+        <div className='flex items-center justify-between'>
+          <p className='font-semibold text-xl'>List storage location</p>
+          <button className='w-[190px] flex items-center justify-between px-3 h-12 bg-white/50 p-1 hover:bg-white/60 rounded-md'>
+            {chain && <ChainIcon chain={chain as ChainWithDetails} className={'h-6 w-6'} />}
+            <p className='text-lg font-semibold truncate'>{chain?.name}</p>
+            <Image src={ArrowDown} alt='Open list storage location chains' className='w-5' />
+          </button>
+        </div>
+        <div className=' flex flex-col gap-1'>
+          <p className='font-semibold text-lg'>Owner</p>
+          <input
+            value={owner}
+            placeholder='Address or ENS name'
+            onChange={e => {
+              if (e.target.value.includes(' ')) return
+              setOwner(e.target.value)
+            }}
+            className='p-3 font-medium truncate rounded-lg w-full bg-white/70'
+          />
+        </div>
+        <div className=' flex flex-col gap-1'>
+          <p className='font-semibold text-lg'>Manager</p>
+          <input
+            value={owner}
+            placeholder='Address or ENS name'
+            onChange={e => {
+              if (e.target.value.includes(' ')) return
+              setOwner(e.target.value)
+            }}
+            className='p-3 font-medium truncate rounded-lg w-full bg-white/70'
+          />
+        </div>
+        <div className=' flex flex-col gap-1'>
+          <p className='font-semibold text-lg'>User</p>
+          <input
+            value={owner}
+            placeholder='Address or ENS name'
+            onChange={e => {
+              if (e.target.value.includes(' ')) return
+              setOwner(e.target.value)
+            }}
+            className='p-3 font-medium truncate rounded-lg w-full bg-white/70'
+          />
+        </div>
+        <div className='w-full mt-10 flex justify-between items-center'>
+          <CancelButton onClick={onClose} label='Exit' />
+          <PrimaryButton
+            label={'Save'}
+            onClick={() => {}}
+            className='text-lg w-32 h-12'
+            disabled={!Object.values(changedValues).includes(true)}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ListSettings
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -10,6 +10,7 @@ import SettingsIcon from 'public/assets/icons/settings.svg'
 import { useEFPProfile } from '#/contexts/efp-profile-context'
 import { UserProfileCard } from '#/components/user-profile-card'
 import { UserProfilePageTable } from '#/components/profile-page-table'
+import ListSettings from './components/ListSettings'
 
 interface Props {
   searchParams: {
@@ -21,6 +22,7 @@ interface Props {
 }
 
 export default function ProfilePage({ searchParams }: Props) {
+  const [listSettingsOpen, setListSettingsOpen] = useState(false)
   const [activeTab, setActiveTab] = useState<ProfileTabType>('following')
 
   const { t } = useTranslation('profile')
@@ -65,52 +67,60 @@ export default function ProfilePage({ searchParams }: Props) {
   }[activeTab]
 
   return (
-    <main className='flex pb-8 min-h-full w-full justify-between xl:justify-center gap-y-4 flex-col md:flex-row flex-wrap xl:flex-nowrap items-start xl:gap-6 mt-32 md:mt-40 lg:mt-48 px-4 lg:px-8'>
-      <div className='flex flex-col w-full xl:w-fit items-center gap-4'>
-        <UserProfileCard profile={profile} following={following} isLoading={profileIsLoading} />
-        <div className='flex flex-col gap-1 items-center'>
-          <p className='font-semibold '>{t('block-mute')}</p>
-          <div className='flex gap-1 cursor-pointer hover:opacity-80 transition-opacity'>
-            <p className='font-semibold '>{t('settings')}</p>
-            <Image src={SettingsIcon} alt='List settings' width={18} height={18} />
+    <>
+      {listSettingsOpen && profile && (
+        <ListSettings profile={profile} onClose={() => setListSettingsOpen(false)} />
+      )}
+      <main className='flex pb-8 min-h-full w-full justify-between xl:justify-center gap-y-4 flex-col md:flex-row flex-wrap xl:flex-nowrap items-start xl:gap-6 mt-32 md:mt-40 lg:mt-48 px-4 lg:px-8'>
+        <div className='flex flex-col w-full xl:w-fit items-center gap-4'>
+          <UserProfileCard profile={profile} following={following} isLoading={profileIsLoading} />
+          <div className='flex flex-col gap-1 items-center'>
+            <p className='font-semibold '>{t('block-mute')}</p>
+            <div
+              className='flex gap-1 cursor-pointer hover:opacity-80 transition-opacity'
+              onClick={() => setListSettingsOpen(true)}
+            >
+              <p className='font-semibold '>{t('settings')}</p>
+              <Image src={SettingsIcon} alt='List settings' width={18} height={18} />
+            </div>
           </div>
         </div>
-      </div>
-      <UserProfilePageTable
-        isLoading={followingIsLoading}
-        following={following}
-        followers={followers}
-        isFetchingMore={isFetchingMoreFollowing}
-        fetchMore={() => fetchMoreFollowing()}
-        canEditTags={true}
-        title='following'
-        customClass='hidden xl:flex'
-      />
-      <UserProfilePageTable
-        isLoading={followersIsLoading}
-        following={following}
-        followers={followers}
-        isFetchingMore={isFetchingMoreFollowers}
-        fetchMore={() => fetchMoreFollowers()}
-        title='followers'
-        customClass='hidden xl:flex'
-      />
-      <div className=' w-full mt-12 relative xl:hidden'>
-        <div className='w-full absolute -top-12 left-0'>
-          {PROFILE_TABS.map(option => (
-            <button
-              key={option}
-              onClick={() => setActiveTab(option)}
-              className={`w-1/2 capitalize text-lg py-2 font-semibold glass-card border-2 border-gray-200 rounded-t-lg ${
-                activeTab === option ? '' : 'bg-black/5'
-              }`}
-            >
-              {t(option)}
-            </button>
-          ))}
+        <UserProfilePageTable
+          isLoading={followingIsLoading}
+          following={following}
+          followers={followers}
+          isFetchingMore={isFetchingMoreFollowing}
+          fetchMore={() => fetchMoreFollowing()}
+          canEditTags={true}
+          title='following'
+          customClass='hidden xl:flex'
+        />
+        <UserProfilePageTable
+          isLoading={followersIsLoading}
+          following={following}
+          followers={followers}
+          isFetchingMore={isFetchingMoreFollowers}
+          fetchMore={() => fetchMoreFollowers()}
+          title='followers'
+          customClass='hidden xl:flex'
+        />
+        <div className=' w-full mt-12 relative xl:hidden'>
+          <div className='w-full absolute -top-12 left-0'>
+            {PROFILE_TABS.map(option => (
+              <button
+                key={option}
+                onClick={() => setActiveTab(option)}
+                className={`w-1/2 capitalize text-lg py-2 font-semibold glass-card border-2 border-gray-200 rounded-t-lg ${
+                  activeTab === option ? '' : 'bg-black/5'
+                }`}
+              >
+                {t(option)}
+              </button>
+            ))}
+          </div>
+          {mobileActiveEl}
         </div>
-        {mobileActiveEl}
-      </div>
-    </main>
+      </main>
+    </>
   )
 }
