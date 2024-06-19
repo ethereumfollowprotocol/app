01cb028457cd9e84bb60b4fcffc7e75b26c3cfb1
encryptedDegen <encrypted@ethfollow.xyz>
2024-06-19 10:28:22 +0200

    Correct RPC endpoint for ENS resolution

--- src/utils/resolveENS.ts
+++ src/utils/resolveENS.ts
@@ -4,27 +4,38 @@ import { createPublicClient, http, type Address } from 'viem'
 
 export const resolveENSProfile = async (address: `0x${string}`) => {
   const resolvedName = await resolveENSName(address)
-  const ensAddress = resolvedName ? await resolveENSAddress(resolvedName) : null
+  // const ensAddress = resolvedName ? await resolveENSAddress(resolvedName) : null
 
   const avatarUrl = `https://metadata.ens.domains/mainnet/avatar/${resolvedName}`
 
   const response = await fetch(avatarUrl)
 
   return {
-    name: address.toLowerCase() === ensAddress?.toLowerCase() ? resolvedName : null,
+    name: resolvedName ?? null,
+    // name: address.toLowerCase() === ensAddress?.toLowerCase() ? resolvedName : null,
     avatar: response.ok ? avatarUrl : undefined
   }
 }
 
 export const resolveENSName = async (address: `0x${string}`) => {
-  const publicClient = createPublicClient({ chain: mainnet, transport: http() })
+  const publicClient = createPublicClient({
+    chain: mainnet,
+    transport: http(
+      `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_MAINNET_ALCHEMY_ID}`
+    )
+  })
   const lookup = await publicClient.getEnsName({ address })
 
   return lookup ?? undefined
 }
 
 export const resolveENSAddress = async (name: string) => {
-  const publicClient = createPublicClient({ chain: mainnet, transport: http() })
+  const publicClient = createPublicClient({
+    chain: mainnet,
+    transport: http(
+      `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_MAINNET_ALCHEMY_ID}`
+    )
+  })
   const ensAddress = await publicClient.getEnsAddress({
     name: normalize(name)
   })
