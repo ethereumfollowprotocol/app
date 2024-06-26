7d4eaf694e17f0262151b6c67584a3a71144679d
encryptedDegen <encrypted@ethfollow.xyz>
2024-06-26 23:33:03 +0200

    Set split size to 100

--- src/hooks/use-checkout.ts
+++ src/hooks/use-checkout.ts
@@ -141,7 +141,7 @@ const useCheckout = () => {
     if (!chainId) return
 
     const splitListOps: CartItem[][] = []
-    const splitSize = 10
+    const splitSize = 100
 
     for (let i = 0; i < cartItems.length; i += splitSize) {
       splitListOps.push(cartItems.slice(i, i + splitSize))
