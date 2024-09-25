df6b3b77d10577cf278bb25819e42eeff405ba76
encryptedDegen <encrypted@ethfollow.xyz>
2024-09-25 12:44:48 +0200

    build fix

--- src/lib/constants/integrations.ts
+++ src/lib/constants/integrations.ts
@@ -2,7 +2,7 @@ import type { StaticImageData } from 'next/image'
 
 import WebHash from 'public/assets/partners/webhash.jpeg'
 import Interface from 'public/assets/partners/interface.jpeg'
-import LikeButton from 'public/assets/partners/likebutton.jpg'
+import LikeButton from 'public/assets/partners/likebutton.jpeg'
 
 export const INTEGRATIONS = [
   {
