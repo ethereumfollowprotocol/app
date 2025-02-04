e06815dc4dcef5acd23d0a485e46b02a66618cdd
encryptedDegen <encrypted@ethfollow.xyz>
2025-02-04 16:14:44 +0100

    parse wallet connect project ID directly

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -36,7 +36,7 @@ const connectors = connectorsForWallets(
   ],
   {
     appName: APP_NAME,
-    projectId: process.env.WALLET_CONNECT_PROJECT_ID || process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID,
+    projectId: 'e726b061ac854a69790d01fa66c76401',
     appDescription: APP_DESCRIPTION,
     appUrl: APP_URL,
     appIcon: 'https://ethfollow.xyz/logo.png',
