93b126ed163599f661c465280623d51d80eebce7
encryptedDegen <encrypted@ethfollow.xyz>
2025-03-10 16:15:43 +0100

    add integration

--- src/lib/constants/integrations.ts
+++ src/lib/constants/integrations.ts
@@ -35,7 +35,7 @@ import Pobstudio from 'public/assets/partners/pobstudio.jpeg'
 import Dhive from 'public/assets/partners/dhive.jpeg'
 import Phi from 'public/assets/partners/phi.png'
 import EFPFinder from 'public/assets/partners/efpfinder.jpeg'
-// import ENSTools from 'public/assets/partners/enstools.jpg'
+import ENSTools from 'public/assets/partners/enstools.jpg'
 
 export const INTEGRATIONS = [
   {
@@ -138,11 +138,11 @@ export const INTEGRATIONS = [
     url: 'https://eth.cd',
     logo: EthDotCd,
   },
-  // {
-  //   name: 'ENS Tools',
-  //   url: 'https://ens.tools',
-  //   logo: ENSTools,
-  // },
+  {
+    name: 'ENS Tools',
+    url: 'https://ens.tools',
+    logo: ENSTools,
+  },
   {
     name: 'EFP Bot',
     url: 'https://x.com/efpbots',
