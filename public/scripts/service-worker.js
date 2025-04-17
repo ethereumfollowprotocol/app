7d63583834a474622f19ccacc8d5479f357840f9
encryptedDegen <encrypted@ethfollow.xyz>
2025-04-17 19:27:44 +0200

    add valid max age, revert service worker changes

--- public/scripts/service-worker.js
+++ public/scripts/service-worker.js
@@ -1,20 +1,17 @@
 self.addEventListener('push', function (event) {
   if (event.data) {
-    try {
-      const data = event.data.json()
-      const options = {
-        body: data.body,
-        icon: data.icon || 'https://efp.app/assets/logo.png',
-        vibrate: [100, 50, 100],
-        data: {
-          dateOfArrival: Date.now(),
-          primaryKey: '2',
-        },
-      }
-      event.waitUntil(self.registration.showNotification(data.title, options))
-    } catch (error) {
-      console.error('Error showing notification:', error)
+    const data = event.data.json()
+    const options = {
+      body: data.body,
+      icon: data.icon || 'https://efp.app/assets/logo.png',
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: '2',
+      },
     }
+
+    event.waitUntil(self.registration.showNotification(data.title, options))
   }
 })
 
