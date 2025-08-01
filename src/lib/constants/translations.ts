6a3e03fcad13bc0415a1e67eaf7ef99fe1c76ad3
encrypted <koyofrontend@protonmail.com>
2025-08-01 17:25:38 +0200

    Add Klingon translation import to ethereum-identity-kit integration

--- src/lib/constants/translations.ts
+++ src/lib/constants/translations.ts
@@ -76,6 +76,7 @@ import te from '../../../public/locales/te/translations.json'
 import th from '../../../public/locales/th/translations.json'
 import tiv from '../../../public/locales/tiv/translations.json'
 import tl from '../../../public/locales/tl/translations.json'
+import tlh from '../../../public/locales/tlh/translations.json'
 import tr from '../../../public/locales/tr/translations.json'
 import tw from '../../../public/locales/tw/translations.json'
 import ug from '../../../public/locales/ug/translations.json'
@@ -169,6 +170,7 @@ export const translations = {
   th,
   tiv,
   tl,
+  tlh,
   tr,
   tw,
   ug,
